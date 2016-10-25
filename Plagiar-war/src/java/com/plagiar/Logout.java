7f71e393e90345723c2a75dace3d8339b2433943
Saimul <Saimul@Saimul>
2016-10-26 01:48:59 +0600

    created repository including download option (modified v2)

--- Plagiar-war/src/java/com/plagiar/Logout.java
+++ Plagiar-war/src/java/com/plagiar/Logout.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.plagiar;
 
 import java.io.IOException;
@@ -19,7 +15,7 @@ public class Logout extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
+        //PrintWriter out = response.getWriter();
 
         request.getSession().invalidate();
         response.sendRedirect(request.getContextPath());
